aa264b7c5d504fa8729d33f452a760b385f0dad3
buryattvoydrug <49764701+buryattvoydrug@users.noreply.github.com>
2021-05-08 20:36:31 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+        $('.screens-slider').slick({
+          slidesToShow: 1,
+          centerMode: true,
+          variableWidth: true,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          infinite: true,
+          dots:false,
+          arrows:false
+        });
+});
